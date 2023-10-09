824f1343821b5ba878141abdf315fd1775085e2e
Samsul Arifin <mas.arif.73@gmail.com>
2023-10-09 16:18:21 +0700

    update
    update dist

---
+++ .nuxt/dist/server/pages/auth.js
@@ -0,0 +1,49 @@
+exports.ids = [7];
+exports.modules = {
+
+/***/ 45:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/auth.vue?vue&type=template&id=0f1fa3fe&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('section', {
+    staticClass: "section"
+  }, [_vm._ssrNode("<div class=\"container\"><h1 class=\"title\">Nuxt Auth</h1></div>")]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./pages/auth.vue?vue&type=template&id=0f1fa3fe&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/auth.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "418c83ae"
+  
+)
+
+/* harmony default export */ var auth = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=auth.js.map
