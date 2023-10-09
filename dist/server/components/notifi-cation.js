7ed6ae12d630a718a916f96552fbe29fb89d2ebf
Samsul Arifin <mas.arif.73@gmail.com>
2023-10-09 16:54:38 +0700

    update
    update dist

---
+++ dist/server/components/notifi-cation.js
@@ -0,0 +1,57 @@
+exports.ids = [2];
+exports.modules = {
+
+/***/ 33:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/NotifiCation.vue?vue&type=template&id=36d5c0ed&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "notification is-danger"
+  }, [_vm._ssrNode(_vm._ssrEscape("\n    " + _vm._s(_vm.message) + "\n"))]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./components/NotifiCation.vue?vue&type=template&id=36d5c0ed&
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/NotifiCation.vue?vue&type=script&lang=js&
+/* harmony default export */ var NotifiCationvue_type_script_lang_js_ = ({
+  name: 'NotifiCation',
+  propos: ['message']
+});
+// CONCATENATED MODULE: ./components/NotifiCation.vue?vue&type=script&lang=js&
+ /* harmony default export */ var components_NotifiCationvue_type_script_lang_js_ = (NotifiCationvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/NotifiCation.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  components_NotifiCationvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "5ac7e7b3"
+  
+)
+
+/* harmony default export */ var NotifiCation = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=notifi-cation.js.map
---
+++ dist/server/components/the-header.js
@@ -0,0 +1,49 @@
+exports.ids = [4];
+exports.modules = {
+
+/***/ 35:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib/loaders/templateLoader.js??ref--6!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./components/TheHeader.vue?vue&type=template&id=7635db12&
+var render = function render() {
+  var _vm = this,
+    _c = _vm._self._c;
+  return _c('div', {
+    staticClass: "container"
+  }, [_vm._ssrNode("<header class=\"blog-header py-3\"><div class=\"row flex-nowrap justify-content-between align-items-center\"><div class=\"col-4 pt-1\"><a href=\"#\" class=\"link-secondary\">Subscribe</a></div> <div class=\"col-4 text center\"><a href=\"#\" class=\"text-dark fs-4\">Articles</a></div> <div class=\"col-4 d-flex justify-content-end align-items-center\"><a href=\"#\" aria-label=\"Search\" class=\"link-secondary\"><svg xmlns:us-gaap=\"<http://www.w3.org/200/svg>\" width=\"20\" height=\"20\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" role=\"img\" viewBox=\"0 0 24 23\" class=\"mx-3\"><title>Search</title><circle cx=\"10.5\" r=\"7.5\"></circle><path d=\"M21 21l-5.2-5.2\"></path></svg></a> <a href=\"#\" class=\"btn btn-sm btn-outline-secondary\"> Sign Up</a></div></div></header>")]);
+};
+var staticRenderFns = [];
+
+// CONCATENATED MODULE: ./components/TheHeader.vue?vue&type=template&id=7635db12&
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./components/TheHeader.vue
+
+var script = {}
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  script,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "17439334"
+  
+)
+
+/* harmony default export */ var TheHeader = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=the-header.js.map
